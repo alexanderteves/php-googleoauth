d9c0182ec2470b4e766edb35abd478144fcf1843
Alexander Teves <alexander.teves@gmail.com>
2013-10-15 14:37:48 +0200

    Completed SqliteStoreTest

--- tests/SqliteStoreTest.php
+++ tests/SqliteStoreTest.php
@@ -7,6 +7,16 @@
         public function setUp() {
             $this->config = parse_ini_file('config.ini');
             $this->sqlitestore = new SqliteStore($this->config);
+            $this->date = new Datetime();
+        }
+
+        /**
+        * @expectedException PDOException
+        * @expectedExceptionMessage identifier is not unique
+        */
+        public function testCreateEntry() {
+            $this->assertTrue($this->sqlitestore->createEntry('testentry', 'abc123', 'def456', $this->date->getTimestamp()));
+            $this->sqlitestore->createEntry('testentry', 'abc123', 'def456', $this->date->getTimestamp());
         }
         
         /**
@@ -15,6 +25,18 @@
         */
         public function testReadEntry() {
             $this->sqlitestore->readEntry('fakeentryidentifier');
+        }
+
+        public function testUpdateEntry() {
+            $this->assertTrue($this->sqlitestore->updateEntry('testentry', 'ghi789', $this->date->getTimestamp()));
+        }
+
+        public function testDeleteEntry() {
+            $this->tearDown();
+        }
+
+        public function tearDown() {
+            $this->assertTrue($this->sqlitestore->deleteEntry('testentry'));
         }
     }
 ?>
